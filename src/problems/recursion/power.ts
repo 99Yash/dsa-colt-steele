dbccb7136d7f0f4c81147281a629ba7f6c388177
yash <yashgouravkar@gmail.com>
2025-11-03 19:28:57 +0530

    ss

---
+++ src/problems/recursion/power.ts
@@ -0,0 +1,6 @@
+///Write a function called power which accepts a base and an exponent. The function should return the power of the base to the exponent
+
+function power(base: number, exp: number): number {
+  if (exp === 0) return 1;
+  return base * power(base, exp - 1);
+}
