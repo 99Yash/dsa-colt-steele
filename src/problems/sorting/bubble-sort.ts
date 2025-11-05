1b8c489f803efb11df13e317de876ee713687243
yash <yashgouravkar@gmail.com>
2025-11-05 20:33:46 +0530

    ss

--- src/problems/sorting/bubble-sort.ts
+++ src/problems/sorting/bubble-sort.ts
@@ -7,13 +7,16 @@ function bubbleSort(arr: number[]) {
   if (arr.length === 1) return arr;
 
   for (let x = 0; x < arr.length; x++) {
+    let noSwaps = true;
     for (let i = 0; i < arr.length - x - 1; i++) {
       if (arr[i]! > arr[i + 1]!) {
         const temp = arr[i]!;
         arr[i] = arr[i + 1]!;
         arr[i + 1] = temp;
+        noSwaps = false;
       }
     }
+    if (noSwaps) break;
   }
 
   return arr;
