1f39df465a04fcf2a6a921658f6bb6b9daf4dc7e
yash <yashgouravkar@gmail.com>
2025-11-06 19:32:26 +0530

    ss

--- src/problems/sorting/insertion-sort.ts
+++ src/problems/sorting/insertion-sort.ts
@@ -1,12 +1,23 @@
 //TODO: Try again
-function insertionSort(arr: number[]) {
+function insertionSort(
+  arr: number[],
+  compareFn?: (s: number, t: number) => number
+) {
   if (arr.length === 0 || arr.length === 1) return arr;
+
+  function defaultComparator(a: number, b: number) {
+    return a - b;
+  }
+
+  if (typeof compareFn !== 'function') {
+    compareFn = defaultComparator;
+  }
 
   for (let i = 1; i < arr.length; i++) {
     const current = arr[i];
     let j = i - 1;
 
-    while (j >= 0 && arr[j]! > current!) {
+    while (j >= 0 && compareFn(current!, arr[j]!) < 0) {
       arr[j + 1] = arr[j] as number;
       j--;
     }
