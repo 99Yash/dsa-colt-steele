5aac49dc47aae5ad243fdf3849a6ce082aa501b3
yash <yashgouravkar@gmail.com>
2025-11-07 19:36:28 +0530

    ss

--- src/problems/sorting/quick-sort.ts
+++ src/problems/sorting/quick-sort.ts
@@ -18,7 +18,12 @@
  * - Returns the final index of the pivot element
  * - All elements left of pivot are smaller, right are larger
  */
-function pivotHelper(arr: number[], start: number, end: number) {
+function pivotHelper(
+  arr: number[],
+  compareFn: (a: number, b: number) => number,
+  start: number,
+  end: number
+) {
   if (start >= arr.length) start = 0;
   if (end >= arr.length) end = arr.length - 1;
 
@@ -28,8 +33,8 @@ function pivotHelper(arr: number[], start: number, end: number) {
   const pivot = arr[start] as number;
   let swapIdx = start;
 
-  for (let i = start + 1; i < end; i++) {
-    if (pivot > arr[i]!) {
+  for (let i = start + 1; i <= end; i++) {
+    if (compareFn(pivot, arr[i]!) > 0) {
       swapIdx++;
       //* increment the pivot index and swap the current element with the pivot index
       const temp = arr[i]!;
@@ -49,8 +54,12 @@ function quickSort(arr: number[], l = 0, r = arr.length - 1) {
   if (arr.length === 0 || arr.length === 1) return arr;
 
   if (l < r) {
+    function defaultComparator(a: number, b: number) {
+      return a - b;
+    }
+
     // l and r are just pointers. if they are the same we've hit the end of one of the branches
-    const pivotIdx = pivotHelper(arr, l, r);
+    const pivotIdx = pivotHelper(arr, defaultComparator, l, r);
     quickSort(arr, l, pivotIdx - 1);
     quickSort(arr, pivotIdx + 1, r);
   }
